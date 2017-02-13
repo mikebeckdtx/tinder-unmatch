50fbdb41d2fd264e97f4ebae25a236214b191872
Jesus Maldonado <jesus.maldonado@datadoghq.com>
2017-02-13 11:48:57 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# tinder-unmatch
+
+This is a pretty simple script to remove any matches from tinder without messages or who haven't logged on in 2017.
+
+## Examples
+
+`git clone` the repo
+
+Step 1: Get your fb token:
+run
+`python tinder.py`
+(you may need to `pip install` some missing packages);
+
+Step 2: Get your fb id from here: http://findmyfbid.com/
+
+Copy+paste these values in `tinder.js` for `FACEBOOK_ACCESS_TOKEN` and `FACEBOOK_ID`
+
+Step 3: run `node index.js`
+
+## Enjoy your
---
+++ index.js
@@ -0,0 +1,29 @@
+const TinderClient = require('./tinder.js')
+const client = new TinderClient();
+client.authorize(
+  "FACEBOOK_ACCESS_TOKEN",
+  'FACEBOOK_ID',
+  function() {
+    client.getHistory(function(error, data){
+        var matches = data.matches;
+        var matchesWithoutMessages = matches.filter(function(match){
+            return match.last_activity_date < '2017-01-01T00:00:00Z' ||
+                match.messages.length === 0;
+        });
+        var idsToUnmatch = matchesWithoutMessages.map(function(match) {
+            return match["_id"];
+        });
+        var interval;
+        var interval = setInterval(function(){
+            var idToUnmatch = idsToUnmatch.splice(0,1);
+            console.log("unmatching" + " " + idToUnmatch);
+            console.log(idsToUnmatch.length + " matches left to unmatch");
+            client.unmatch(idToUnmatch, function(){
+                console.log("left: " + idsToUnmatch.length);
+            });
+            if (idsToUnmatch.length === 0) {
+                    clearInterval(interval);
+            }
+        }, 1000)
+    });
+});
